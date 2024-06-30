a4ae0a49070625e0d03b7d40990562be8efc856c
eilujoshua <eilujoshua200@gmail.com>
2024-06-30 04:05:22 -0400

    great

--- app/categories/selected_all_categories/page.js
+++ app/categories/selected_all_categories/page.js
@@ -4,7 +4,7 @@ import {useSearchParams} from 'next/navigation';
 import {useData} from '../../helpers/data.js';
 export default function SelectedAllCategories(){
     const {contentData} = useData();
-    const category = new URLSearchParams(useSearchParams()).get('category' || null);
+    const category = new URLSearchParams(useSearchParams()).get('category');
     const CategoryData = contentData.filter(meal => meal.strCategory === category);
     return (
         <div>
