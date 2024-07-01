97ad69ab13dd90653ec90a5120655615e453bd28
eiluj <eilujoshua200@gmail.com>
2024-07-01 07:04:06 -0400

    all meals

--- app/all_meals/page.js
+++ app/all_meals/page.js
@@ -2,16 +2,32 @@
 import {useData} from '../helpers/data';
 import Meals from '../components/meals';
 export default function AllMeals() {
-  const fetcher = (...args) => fetch(...args).then(res => res.json());
-  const { data, error } = useSWR('https://www.themealdb.com/api/json/v2/9973533/search.php?f=b', fetcher);
+  
 
+  
   const {setContentData, contentData} = useData();
   
-  useEffect(() => {
-    if (data?.meals && data.meals.length > 0) {
-      setContentData(data.meals);
-    }
-  }, [data]);
+    const fetchData = async () => {
+      try {
+        const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+        const meals = [];
+
+        for (let letter of alphabet) {
+          const response = await fetch(`https://www.themealdb.com/api/json/v2/9973533/search.php?f=${letter}`);
+          const data = await response.json();
+          if (data?.meals && data.meals.length > 0) {
+            meals.push(...data.meals);
+          }
+        }
+
+        setContentData(meals);
+      }
+      catch (error) {
+        console.error('Error:', error);
+      }
+    };
+
+    fetchData();
 
   return (
     <div>
